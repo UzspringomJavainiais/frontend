444f28a9acf7005899941b865c138eb02caa9044
Rokas Ulickas <rokas@hcm.lt>
2019-05-07 11:03:27 +0300

    add new trip component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { TripsComponent } from './components/trips/trips.component';
-import { TripDetailsComponent } from './components/trip-details/trip-details.component';
+import { TripsComponent } from './components/trip/trips/trips.component';
+import { TripDetailsComponent } from './components/trip/trip-details/trip-details.component';
 import {AllApartmentsComponent} from './components/apartments/all-apartments/all-apartments.component';
 import {AllEmployeesComponent} from './components/employes/all-employees/all-employees.component';
 import {StatisticsComponent} from './components/statistics/statistics.component';
+import {NewTripComponent} from './components/trip/new-trip/new-trip.component';
 
 const routes: Routes = [
     { path: 'trips', component: TripsComponent },
     { path: 'trip-details/:id', component: TripDetailsComponent },
+    { path: 'trips/new', component: NewTripComponent },
     { path: 'apartments', component: AllApartmentsComponent},
     { path: 'employees', component: AllEmployeesComponent},
     { path: 'statistics', component: StatisticsComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import {NavbarComponent} from './components/navbar/navbar.component';
-import {TripsComponent} from './components/trips/trips.component';
-import {TripDetailsComponent} from './components/trip-details/trip-details.component';
+import {TripsComponent} from './components/trip/trips/trips.component';
+import {TripDetailsComponent} from './components/trip/trip-details/trip-details.component';
 import {MyTripsComponent} from './components/my-trips/my-trips.component';
 import {TripsService} from './trips.service';
 import {HttpClientModule} from '@angular/common/http';
@@ -20,6 +20,7 @@ import {NewEmployeeModalComponent} from './components/employes/new-employee-moda
 import {EmployeeInfoComponent} from './components/employes/employee-info/employee-info.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import {ChartsModule} from 'ng2-charts';
+import { NewTripComponent } from './components/trip/new-trip/new-trip.component';
 
 @NgModule({
     declarations: [
@@ -34,7 +35,8 @@ import {ChartsModule} from 'ng2-charts';
         AllEmployeesComponent,
         NewEmployeeModalComponent,
         EmployeeInfoComponent,
-        StatisticsComponent
+        StatisticsComponent,
+        NewTripComponent
     ],
     imports: [
         BrowserModule,
