848f7d0861b5227cba1fa22f62056df984352fdc
Rokas Ulickas <ulickas.rokas@gmail.com>
2019-05-30 22:43:35 +0300

    imprvs to statistics add date adapter for correct dates when picking from datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {Injectable, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -30,8 +30,46 @@ import {MyOrganisedTripsComponent} from './components/main/trip/administrate-tri
 import {MyTripsComponent} from './components/main/trip/my-trips/my-trips.component';
 import {MergeTripsModalComponent} from './components/main/trip/administrate-trips/merge-trips-modal/merge-trips-modal.component';
 import {EditTripComponent} from './components/main/trip/administrate-trips/edit-trip/edit-trip.component';
-import {MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DIALOG_DEFAULT_OPTIONS, NativeDateAdapter} from '@angular/material';
 import { AllTripsComponent } from './components/main/trip/administrate-trips/all-trips/all-trips.component';
+import {MomentUtcDateAdapter} from './_adapters/AppDateAdapter';
+import {MAT_MOMENT_DATE_FORMATS} from '@angular/material-moment-adapter';
+
+@Injectable()
+export class MyDateAdapter extends NativeDateAdapter {
+    getFirstDayOfWeek(): number {
+        return 1;
+    }
+
+    format(date: any, displayFormat: Object): string {
+        if (displayFormat === 'input') {
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            if (day < 10) {
+                day = '0' + day;
+            }
+            if (month < 10) {
+                month = '0' + month;
+            }
+            return `${year}-${month}-${day}`;
+        }
+
+        return date.toDateString();
+    }
+}
+
+export const APP_DATE_FORMATS = {
+    parse: {
+        dateInput: {month: 'short', year: 'numeric', day: 'numeric'},
+    },
+    display: {
+        dateInput: 'input',
+        monthYearLabel: {year: 'numeric', month: 'numeric'},
+        dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+        monthYearA11yLabel: {year: 'numeric', month: 'long'},
+    }
+};
 
 @NgModule({
     declarations: [
@@ -71,6 +109,8 @@ import { AllTripsComponent } from './components/main/trip/administrate-trips/all
     ],
     providers: [
         CookieService,
+        {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+        {provide: DateAdapter, useClass: MomentUtcDateAdapter},
         {
             provide: HTTP_INTERCEPTORS,
             useClass: TokenInterceptor,
--- src/app/components/main/statistics/statistics.component.ts
+++ src/app/components/main/statistics/statistics.component.ts
@@ -3,6 +3,8 @@ import {ChartDataSets, ChartOptions} from 'chart.js';
 import {BaseChartDirective, Color, Label} from 'ng2-charts';
 import * as pluginAnnotations from 'chartjs-plugin-annotation';
 import {TripService} from '../../../_services/trip.service';
+import {StatisticsService} from '../../../_services/statistics.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
     selector: 'app-statistics',
@@ -16,22 +18,91 @@ export class StatisticsComponent implements OnInit {
         scaleShowVerticalLines: false,
         responsive: true
     };
-    public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+
+    public barChartOptions3 = {
+        scaleShowVerticalLines: false,
+        responsive: true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    reverse: false,
+                    stepSize: 1,
+                    beginAtZero: true
+                },
+            }]
+        }
+
+    };
     public barChartType = 'bar';
     public barChartLegend = true;
-    public barChartData = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-    ];
 
-    // @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+    // public barChartData = [{data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    public barChartData1 = [{data: [], label: 'Trip days'}];
+    public barChartLabels1: any[];
 
-    constructor(private tripService: TripService) {
+    public barChartData2 = [{data: [], label: 'Trip price'}];
+    public barChartLabels2: any[];
+
+    public barChartData3 = [{data: [], label: 'Trips'}];
+    public barChartLabels3: any[];
+
+    public barChartData4 = [{data: [], label: 'Trip days'}];
+    public barChartLabels4 = ['Trip count'];
+    
+    public dateFromForm: FormGroup;
+
+
+    constructor(private statisticsService: StatisticsService,
+                private fb: FormBuilder) {
     }
 
     ngOnInit() {
-        this.tripService.getTrips()
-            .subscribe(data => console.log(data));
+        this.dateFromForm = this.fb.group({
+            dateFrom: ['', Validators.required],
+            dateTo: ['', Validators.required],
+        });
+
+        this.dateFromForm.valueChanges
+            .subscribe(data => {
+                if (!this.dateFromForm.value.dateFrom || !this.dateFromForm.value.dateTo) {
+                    console.log('invalid dates');
+                    return;
+                }
+                this.statisticsService
+                    .getTripCountByPeriod(this.dateFromForm.value.dateFrom, this.dateFromForm.value.dateTo)
+                    .subscribe(data => this.barChartData4[0].data = [data]);
+            });
+
+        this.statisticsService.getTripsByDuration()
+            .subscribe((data: any[]) => {
+                this.barChartLabels1 = data.map(item => `Trip name: ${item.name}`);
+                this.barChartData1[0].data = data.map(item => item.dateDiff);
+                console.log('getTripsByDuration: ', data);
+            });
+
+        this.statisticsService.getTripsByPrice()
+            .subscribe((data: any[]) => {
+                this.barChartLabels2 = data.map(item => `Trip name: ${item.name}`);
+                this.barChartData2[0].data = data.map(item => item.sum);
+                console.log('getTripsByPrice: ', data);
+            });
+
+        this.statisticsService.getTripCountByEmployee()
+            .subscribe((data: any[]) => {
+                this.barChartLabels3 = data.map(item => `Employee name: ${item.name}`);
+                this.barChartData3[0].data = data.map(item => item.sum);
+                console.log('getTripCountByEmployee: ', data);
+            });
+
+        // this.statisticsService.getTripCountByPeriod()
+        //     .subscribe((data: any[]) => {
+        //         this.barChartLabels4 = ['Trip count'];
+        //         this.barChartData4[0].data = [data];
+        //         console.log('getTripCountByPeriod: ', data);
+        //     });
+
+        this.statisticsService.getTripsByApartments()
+            .subscribe((data: any[]) => console.log(data));
     }
 
 }
