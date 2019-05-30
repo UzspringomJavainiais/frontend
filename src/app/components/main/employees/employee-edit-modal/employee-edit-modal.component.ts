e4cea0499c6bf9a4a3b99103c6a7ee5767b8b236
Rokas Ulickas <ulickas.rokas@gmail.com>
2019-05-31 02:07:36 +0300

    asdas

--- src/app/components/main/employees/employee-edit-modal/employee-edit-modal.component.ts
+++ src/app/components/main/employees/employee-edit-modal/employee-edit-modal.component.ts
@@ -1,56 +1,58 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { EmployeeService } from 'src/app/_services/employee.service';
-import { RoleService } from 'src/app/_services/role.service';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { Employee } from '../../../../models/Employee';
+import {Component, OnInit, Inject} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {EmployeeService} from 'src/app/_services/employee.service';
+import {RoleService} from 'src/app/_services/role.service';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {Employee} from '../../../../models/Employee';
 
 @Component({
-  selector: 'app-employee-edit-modal',
-  templateUrl: './employee-edit-modal.component.html',
-  styleUrls: ['./employee-edit-modal.component.css']
+    selector: 'app-employee-edit-modal',
+    templateUrl: './employee-edit-modal.component.html',
+    styleUrls: ['./employee-edit-modal.component.css']
 })
 export class EmployeeEditModalComponent implements OnInit {
-  public editEmployeeForm: FormGroup;
-  public selectedEmployee: Employee;
-  public roles: any;
-  id: number;
+    public editEmployeeForm: FormGroup;
+    public selectedEmployee: Employee;
+    public roles: any;
+    id: number;
 
-  constructor(
-    private employeeService: EmployeeService,
-    private fb: FormBuilder,
-    private roleService: RoleService,
-    public dialogRef: MatDialogRef<EmployeeEditModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) {
-    this.selectedEmployee = data;
-  }
+    constructor(private employeeService: EmployeeService,
+                private fb: FormBuilder,
+                private roleService: RoleService,
+                public dialogRef: MatDialogRef<EmployeeEditModalComponent>,
+                @Inject(MAT_DIALOG_DATA) public data: any) {
 
-  ngOnInit() {
-    this.editEmployeeForm = this.fb.group({
-      id: [this.selectedEmployee.id],
-      firstName: [this.selectedEmployee.firstName, Validators.required],
-      lastName: [this.selectedEmployee.lastName, Validators.required],
-      password: ['', Validators],
-      email: [this.selectedEmployee.email, Validators.required],
-      roles: ['', Validators]
-    });
-    this.getRoles();
-  }
+        this.selectedEmployee = data;
+    }
 
-  saveEmployee() {
-    const employee = this.editEmployeeForm.value;
-    employee.roleIds = employee.roles.map(role => role.id);
-    console.log('išsaugomas: ', employee);
-    this.employeeService
-      .editEmployee(employee)
-      .subscribe(data => {
-        console.log('Edited employee');
-        this.dialogRef.close();
-      });
-  }
+    ngOnInit() {
+        console.log(this.selectedEmployee);
+        this.editEmployeeForm = this.fb.group({
+            id: [this.selectedEmployee.id],
+            firstName: [this.selectedEmployee.firstName, Validators.required],
+            lastName: [this.selectedEmployee.lastName, Validators.required],
+            password: ['', Validators],
+            email: [this.selectedEmployee.email, Validators.required],
+            roles: ['', Validators]
+        });
+        this.getRoles();
+    }
 
-  private getRoles() {
-    this.roleService.getAllRoles().subscribe(data => (this.roles = data));
-  }
+    saveEmployee() {
+        const employee = this.editEmployeeForm.value;
+        this.employeeService
+            .editEmployee(employee)
+            .subscribe(data => {
+                console.log('Edited employee');
+                this.dialogRef.close();
+            });
+    }
+
+    private getRoles() {
+        this.roleService.getAllRoles()
+            .subscribe(data => {
+                this.roles = data;
+                this.editEmployeeForm.patchValue({roles: data});
+            });
+    }
 }
